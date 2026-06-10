254f64c734349415e2cfe081a12d5dc78fb53ff6
unknown <pf95fabio@gmail.com>
2026-06-10 16:14:19 -0300

    chore: configuração do github pages e actions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/sonorize_web/', // Necessário para o GitHub Pages achar os arquivos
   plugins: [react()],
   server: {
     port: 3000,
